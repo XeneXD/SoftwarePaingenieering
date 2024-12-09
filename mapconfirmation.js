67ee96b63a30ee7ddc91f1aac7b01f17a1192b20
Joshua Enad <152776204+XeneXD@users.noreply.github.com>
2024-12-09 04:20:07 -0800

    Add files via upload

---
+++ mapconfirmation.js
@@ -0,0 +1,6 @@
+document.getElementById('donation-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Logic for map confirmation goes here
+    alert('Proceeding to map confirmation...');
+    // Redirect to map confirmation page or display map
+  });
